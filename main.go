6d62a63be3b85775b91ef6d2495b9d87a7803793
ANTRIKSH-GANJOO <aniganjoo@gmail.com>
2023-06-19 00:13:22 -0700

    Go it is

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var conferencename = "Go Conference"
+
+	const totaltickets = 50
+
+	var remaining = 50
+
+	var bookings [50]string
+
+	fmt.Println("Hello", conferencename)
+	fmt.Println("We have total of", totaltickets, "tickets")
+	fmt.Println("Remaining tickets", remaining)
+
+	var username string
+
+	var usertickets int
+	fmt.Printf("Enter your Name:")
+	fmt.Scan(&username)
+	fmt.Printf("Enter number of tickets:")
+	fmt.Scan(&usertickets)
+
+	fmt.Println(username)
+	bookings[0] = username
+	fmt.Printf("the whole array: %v\n", bookings)
+	fmt.Printf("the first value: %v\n", bookings[0])
+	fmt.Println(usertickets)
+
+	remaining = remaining - usertickets
+
+	fmt.Printf("%v tickets remaining for %v\n", remaining, conferencename)
+
+}
